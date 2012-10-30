611fbb34b750164463564aaa2861ba0785b984a9
Rick Waldron <waldron.rick@gmail.com>
2012-10-30 11:17:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# schedule
+
+Non-blocking, temporal task sequencing.
+
+Note: `schedule` does NOT use `setTimeout` or `setInterval`
+
+## Getting Started
+
+```bash
+npm install schedule
+```
+
+
+## Examples
+
+```javascript
+var schedule = require('schedule');
+
+// Wait n milliseconds, execute a task
+schedule.wait( 500, function() {
+
+  console.log( "500ms later..." );
+
+});
+
+// Loop every n milliseconds, executing a task each time
+schedule.loop( 500, function( loop ) {
+
+  console.log( "Every 500ms..." );
+
+  // `loop` is a reference to the schedule instance
+  // use it to cancel the loop by calling:
+  //
+  loop.stop();
+
+  // The number of times this loop has been executed:
+  loop.called; // number
+
+});
+
+// Queue a sequence of tasks: wait, wait
+// Each wait time is added to the prior wait times.
+schedule.queue([
+  {
+    wait: 500,
+    task: function() {
+      // Executes 500ms after schedule.queue(...) is called
+    }
+  },
+  {
+    wait: 500,
+    task: function() {
+      // Executes 1000ms after schedule.queue(...) is called
+
+      // The last "wait" task will emit an "ended" event
+    }
+  }
+]);
+
+// Queue a sequence of tasks: wait then loop
+// Each wait time is added to the prior wait times.
+schedule.queue([
+  {
+    wait: 500,
+    task: function() {
+      // Executes 500ms after schedule.queue(...) is called
+    }
+  },
+  {
+    loop: 100,
+    task: function() {
+      // Executes 600ms after schedule.queue(...) is called
+
+      // Executes every 100ms thereafter.
+    }
+  }
+]);
+```
+
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/gruntjs/grunt).
+
+## License
+Copyright (c) 2012 Rick Waldron
+Licensed under the MIT license.
---
+++ test/test.js
@@ -0,0 +1,271 @@
+var schedule = require("../lib/schedule.js"),
+    events = require("events"),
+    util = require("util");
+
+
+function sum( a, b ) {
+  return a + b;
+}
+
+function fuzzy( actual, expected, tolerance ) {
+  var diff;
+
+  tolerance = tolerance === undefined ? 10 : tolerance;
+
+  if ( actual === expected ) {
+    return true;
+  }
+  diff = fuzzy.lastDiff = Math.abs( actual - expected );
+
+  if ( diff <= tolerance ) {
+    return true;
+  }
+  return false;
+}
+
+fuzzy.lastDiff = 0;
+
+
+
+exports[ "context" ] = {
+  setUp: function( done ) {
+    done();
+  },
+  loop: function( test ) {
+    test.expect(1);
+
+    schedule.loop( 200, function( context ) {
+      console.log( context );
+
+      test.ok( context === this );
+
+      if ( context.called === 1 ) {
+        this.stop();
+        test.done();
+      }
+    });
+  }
+};
+
+
+exports[ "loops" ] = {
+  setUp: function( done ) {
+    done();
+  },
+  stop: function( test ) {
+    test.expect(1);
+
+    var scheduledAt, completeds, last;
+
+    scheduledAt = Date.now();
+
+    completeds = [];
+
+    schedule.loop( 100, function( loop ) {
+      // console.log( "a", a );
+      if ( loop.called === 1 ) {
+        completeds.push( loop.called );
+        this.stop();
+      }
+    });
+
+    schedule.loop( 100, function( loop ) {
+      // console.log( "b", b );
+      if ( loop.called === 3 ) {
+        completeds.push( loop.called );
+        this.stop();
+      }
+    });
+
+    last = schedule.loop( 100, function( loop ) {
+      // console.log( "c", c );
+      if ( loop.called === 5 ) {
+        completeds.push( loop.called );
+        loop.stop();
+      }
+    });
+
+
+    last.on("stop", function() {
+      var result = completeds.reduce( sum, 0 );
+
+      test.equal( result, 9, "sum of loop.called counters is 9" );
+      test.done();
+    });
+  }
+};
+
+
+
+exports[ "wait" ] = {
+  setUp: function( done ) {
+    done();
+  },
+  wait: function( test ) {
+    test.expect(7);
+
+    var completed, times;
+
+    completed = 0;
+
+    times = [
+      10, 100, 150, 500, 750, 1000, 3000
+    ];
+
+    times.forEach(function( time, k ) {
+
+      var scheduledAt = Date.now(),
+          expectAt = scheduledAt + time;
+
+      schedule.wait( time, function( wait ) {
+        var actual = Date.now();
+
+        test.ok(
+          fuzzy( actual, expectAt ),
+          "time: " + time + " ( " + Math.abs( actual - expectAt ) + ")"
+        );
+
+        if ( ++completed === times.length ) {
+          test.done();
+        }
+        console.log( completed, time );
+      });
+    });
+  }
+};
+
+
+exports[ "queue" ] = {
+  setUp: function( done ) {
+    done();
+  },
+  wait: function( test ) {
+    test.expect(3);
+
+    var scheduledAt = Date.now(),
+        expectAt = scheduledAt + 100;
+
+    // Wait queue
+    schedule.queue([
+      {
+        wait: 100,
+        task: function() {
+          var now = Date.now();
+
+          test.ok( fuzzy(now, expectAt, 1), "queued fn 1: on time" );
+          expectAt = now + 200;
+        }
+      },
+      {
+        wait: 200,
+        task: function( task ) {
+          var now = Date.now();
+
+          test.ok( fuzzy(now, expectAt, 1), "queued fn 2: on time" );
+          test.equal( now, scheduledAt + 300, "queue lapse correct" );
+
+          test.done();
+        }
+      }
+    ]);
+  },
+  loop: function( test ) {
+    test.expect(6);
+
+    var scheduledAt = Date.now(),
+        expectAt = scheduledAt + 100;
+
+    // Wait queue
+    schedule.queue([
+      {
+        wait: 100,
+        task: function( task ) {
+          var now = Date.now();
+
+          test.ok( fuzzy(now, expectAt, 1), "queued wait fn 1: on time" );
+          expectAt = now + 200;
+        }
+      },
+      {
+        loop: 200,
+        task: function( task ) {
+          var now = Date.now();
+
+          if ( task.called === 1 ) {
+            test.ok( fuzzy(now, expectAt, 1), "queued loop fn 1: on time" );
+            test.ok( fuzzy(now, scheduledAt + 300, 1), "queue lapse correct" );
+          }
+
+          if ( task.called === 2 ) {
+            test.ok( "stop" in task );
+            test.ok( fuzzy(now, expectAt, 1), "queued loop fn 2: on time" );
+            test.ok( fuzzy(now, scheduledAt + 500, 1), "queue lapse correct" );
+            test.done();
+          }
+
+          expectAt = now + 200;
+        }
+      }
+    ]);
+  },
+  end: function( test ) {
+    test.expect(3);
+
+    var queue;
+
+    // Wait queue
+    queue = schedule.queue([
+      {
+        wait: 100,
+        task: function() {
+          test.ok( true );
+        }
+      },
+      {
+        wait: 100,
+        task: function() {
+          test.ok( true );
+        }
+      }
+    ]);
+
+    queue.on("ended", function() {
+      test.ok( true );
+      test.done();
+    });
+  }
+};
+
+
+Object.keys(exports).forEach(function( exp ) {
+  var setUp = exports[ exp ].setUp;
+  exports[ exp ].setUp = function( done ) {
+    console.log( "\n" );
+    setUp( done );
+  };
+});
+
+
+
+
+
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
