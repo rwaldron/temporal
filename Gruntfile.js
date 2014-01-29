bf01d133f1a3119c55bda8b0dad9a705acb409fa
Rick Waldron <waldron.rick@gmail.com>
2014-01-29 11:31:19 -0500

    Adds jsbeautifier
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,16 +20,50 @@ module.exports = function(grunt) {
         options: readOptionalJSON(".jshintrc")
       }
     },
+    jsbeautifier: {
+      files: ["lib/**/*.js", "eg/**/*.js", "test/**/*.js"],
+      options: {
+        js: {
+          braceStyle: "collapse",
+          breakChainedMethods: false,
+          e4x: false,
+          evalCode: false,
+          indentChar: " ",
+          indentLevel: 0,
+          indentSize: 2,
+          indentWithTabs: false,
+          jslintHappy: false,
+          keepArrayIndentation: false,
+          keepFunctionIndentation: false,
+          maxPreserveNewlines: 10,
+          preserveNewlines: true,
+          spaceBeforeConditional: true,
+          spaceInParen: false,
+          unescapeStrings: false,
+          wrapLineLength: 0
+        }
+      }
+    },
     watch: {
-      files: "<config:jshint.files>",
-      tasks: "default"
+      src: {
+        files: [
+          "Gruntfile.js",
+          "lib/**/*.js",
+          "test/**/*.js"
+        ],
+        tasks: ["default"],
+        options: {
+          interrupt: true,
+        },
+      }
     }
   });
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-nodeunit");
+  grunt.loadNpmTasks("grunt-jsbeautifier");
   // Default task.
-  grunt.registerTask( "default", [ "jshint", "nodeunit" ] );
+  grunt.registerTask( "default", [ "jsbeautifier", "jshint", "nodeunit" ] );
 
 
 };
